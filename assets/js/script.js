bbdaeffe96461d6bb1acf2e397de787b16555be3
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-03 16:06:11 +0100

    TASK 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,42 @@ for (var property in student) {
 }
 
 //TASK 2 Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+var students = [
+  {
+    name: "Ajeje",
+    lastName: "Brazorf",
+    age: 99,
+  },
+  {
+    name : "Brambilla",
+    lastName : "Fumagalli",
+    age : 1,
+  },
+  {
+    name : "Conte",
+    lastName : "Mascetti",
+    age : 50,
+  },
+];
+
+
+// console.log(students);
+
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+
+
+for (var propriety in students) {
+ 
+  var studentName = students[propriety].name;
+  console.log(students[propriety].name);
+  console.log(students[propriety].lastName);
+}
+
+
+
 //TASK 3 Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
